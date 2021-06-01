a4a9bcdc13c40a1901b87e659e0f247518fb8cca
Sarah Iraurgui <iraurguisj@gmail.com>
2021-05-31 21:21:37 -0400

    contact area created and styled

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const hamburgerButton = document.getElementById("hamburger")
+const navList = document.getElementById("nav-list")
+
+function toggleButton() {
+    navList.classList.toggle("show")
+}
